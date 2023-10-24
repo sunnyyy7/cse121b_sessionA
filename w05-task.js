984f2c07d9aa49e3ebe0808c0a07763990f268ff
yourusername <youremail@>
2023-10-23 21:58:21 -0600

    Finised Task 5

---
+++ w05-task.js
@@ -0,0 +1,48 @@
+/* W05: Programming Tasks */
+const templesElement = document.quarySelector('temples');
+let templelist = [];
+
+const displayTemples = (temples) => {
+temples.forEach(temple => {
+    let article = document.createElement('article');
+    let templeName = document.createElement('h3')
+    templeName.textContent = temple.templeName;
+    let img = document.createElement('img');
+    img.setAttribute('src', temple.imageUrl);
+    img.setAttribute('alt', temple.location);
+    
+    article.appendChild(templeName);
+    article.appendChild(img);
+
+    templesElement.appendChild(article);
+});
+}
+
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    templelist = await response.json();
+    displayTemples(templeList);
+};
+
+
+/* Declare and initialize global variables */
+
+
+/* async displayTemples Function */
+
+
+
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* sortBy Function */
+
+
+
+getTemples();
+
+/* Event Listener */
