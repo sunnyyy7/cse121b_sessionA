7a27771a2adf160aee514ec4e814724891e37ad2
yourusername <youremail@>
2023-10-24 20:52:12 -0600

    Task 4 Js updated

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -2,85 +2,79 @@
 
 /* Profile Object  */
 
-let myprofile = {
-
-/* Populate Profile Object with placesLive objects */
-    myProfile.placeLived.push(
-        {
-            place: 'Rexburg, US',
-            length: '3 years',
-            placemission: 'Toronto, CA',
-            length: '2 years',
-            born:'Hong Kong',
-            length: '20years',
-            
-        }
-    );
-
-/* DOM Manipulation - Output */
-
-/* Name */
-    name: "Sunny Yeung",
-
-    document.querySelector('#name'). textContent = myProfile.name;
-    
-/* Photo with attributes */
+let myInfo = {
+    /* Populate Profile Object with placesLive objects */
+    /* DOM Manipulation - Output */
+    /* Name */
+    name: 'Sunny Yeung',
+    /* Photo with attributes */
     photo: 'images/IMG_1204 copy.jpg',
-
-    document.querySelector('#photo'). textContent = 'src', profilePicture;
-    document.querySelector('#name'). textContent = 'alt','images/IMG_1204 copy.jpg';
-
-/* Favorite Foods List*/
+    /* Favorite Foods List*/
     favoriteFoods: [
-        'DimSum', 
-        'Fried Rice', 
-        'Noddles', 
-        'Pizza', 
+        'DimSum',
+        'Fried Rice',
+        'Noddles',
+        'Pizza',
         'Salad'
     ],
-
-    myProfile.favriteFoods.forEach(food => {
-        let li = document.createElement ('li');
-        li. etxtContent = food;
-        document.querySelector('#favorite-foods') .appendChild(li);
-    });
-
-/* Hobbies List */
+    /* Hobbies List */
     hobbies: [
         'Basketball',
         'Football',
         'pickle ball',
-        'Outdoor activity',
         'Board games',
+        'Camping',
+        'hiking',
+        'backpacking',
         'Travel'
 
-    ],
-
-    myProfile.hobbies.forEach(food => {
-        let li = document.createElement ('li');
-        li. etxtContent = food;
-        document.querySelector('#hobbies') .appendChild(li);
-    });
-
+    ]
+};
 /* Places Lived DataList */
-    placesLived: [
-    ],
+myInfo.placesLived = [
+    {
+        place: 'Rexburg, ID',
+        length: '3 years'
+    },
+    {
+        place: 'Toronto, CA',
+        length: '2 years'
+    },
+    {
+        place: 'Hong Kong',
+        length: '20 year'
+    }
+]
 
-    myProfile.placesLived.forEach(place => {
-        let dt = document.createElement ('dt');
-        dt. etxtContent = [place];
-        document.querySelector('#places-lived') .appendChild(dt);
-    });
+document.querySelector('#name').textContent = myInfo.name;
 
-    myProfile.placesLived.forEach(length => {
-        let dd = document.createElement ('dd');
-        dd. etxtContent = length;
-        document.querySelector('#places-lived') .appendChild(dd);
-    });
+document.querySelector('#photo').setAttribute('src', myInfo.photo);
 
-    myProfile.placesLived.forEach(born => {
-        let dl = document.createElement ('dl');
-        dl. etxtContent = born;
-        document.querySelector('#places-lived') .appendChild(dl);
-    });
-},
+document.querySelector('#photo').setAttribute('alt', myInfo.name);
+
+myInfo.favoriteFoods.forEach((food) => {
+    let favoriteFoodElem = document.createElement('li');
+    favoriteFoodElem.textContent = food;
+
+    document.querySelector('#favorite-foods').appendChild(favoriteFoodElem);
+});
+
+myInfo.hobbies.forEach((hobby) => {
+    let hobbyElem = document.createElement('li');
+    hobbyElem.textContent = hobby;
+
+    
+    document.querySelector('#hobbies').appendChild(hobbyElem);
+});
+
+myInfo.placesLived.forEach((placeLived) => {
+    let placesLivedPlace = document.createElement('dt');
+    placesLivedPlace.textContent = placeLived.place;
+    
+    let placesLivedLength = document.createElement('dd');
+    placesLivedLength.textContent = placeLived.length;
+    
+    document.querySelector('#places-lived').appendChild(placesLivedPlace);
+    document.querySelector('#places-lived').appendChild(placesLivedLength);
+})
+
